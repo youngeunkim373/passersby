e313edac7f6a649b51fcdc5102f7d4f0a7d8fac1
zeroequaltwo <youngeunkim373@gmail.com>
2024-10-09 00:38:18 +0900

    feat: [PB-7] install and set up eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import * as stylex from "@stylexjs/stylex";
+import Image from 'next/image';
+import styles from './page.module.css';
+import * as stylex from '@stylexjs/stylex';
 
-export default function Home() {
+export default function Home( ) {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <Image
           className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
+          src={'https://nextjs.org/icons/next.svg'}
+          alt={'Next.js logo'}
           width={180}
           height={38}
           priority
@@ -24,23 +24,23 @@ export default function Home() {
         <div className={styles.ctas}>
           <a
             className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href={'https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'}
+            target={'_blank'}
+            rel={'noopener noreferrer'}
           >
             <Image
               className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
+              src={'https://nextjs.org/icons/vercel.svg'}
+              alt={'Vercel logomark'}
               width={20}
               height={20}
             />
             Deploy now
           </a>
           <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href={'https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'}
+            target={'_blank'}
+            rel={'noopener noreferrer'}
             className={styles.secondary}
           >
             Read our docs
@@ -49,42 +49,42 @@ export default function Home() {
       </main>
       <footer className={styles.footer}>
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href={'https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'}
+          target={'_blank'}
+          rel={'noopener noreferrer'}
         >
           <Image
             aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
+            src={'https://nextjs.org/icons/file.svg'}
+            alt={'File icon'}
             width={16}
             height={16}
           />
           Learn
         </a>
         <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href={'https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'}
+          target={'_blank'}
+          rel={'noopener noreferrer'}
         >
           <Image
             aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
+            src={'https://nextjs.org/icons/window.svg'}
+            alt={'Window icon'}
             width={16}
             height={16}
           />
           Examples
         </a>
         <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href={'https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'}
+          target={'_blank'}
+          rel={'noopener noreferrer'}
         >
           <Image
             aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
+            src={'https://nextjs.org/icons/globe.svg'}
+            alt={'Globe icon'}
             width={16}
             height={16}
           />
@@ -97,7 +97,7 @@ export default function Home() {
 
 const testStyles = stylex.create({
   test: {
-    color: 'blue',
     background: 'pink',
+    color: 'blue',
   }
-}); 
+});
