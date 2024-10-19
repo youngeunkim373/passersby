4fcbbc999abe12e0169e9321ca8e7f7e6f0273b0
0=2 dev <104338021+youngeunkim373@users.noreply.github.com>
2024-10-19 22:07:48 +0900

    feat: [PB-10] Alert 컴포넌트 개발 (#6)
    * feat: [PB-10] create useVisible custom hook
    
    * feat: [PB-10] create Alert component
    
    * feat: [PB-10] fix Alert UI and add click event to Button Storybook doc
    
    * feat: [PB-10] add orange colot palette
    
    * trivial: [PB-10] remove page from storybook

---
+++ src/assets/icons/Close.tsx
@@ -0,0 +1,32 @@
+export function Close() {
+  return (
+    <svg 
+      className={'w-3 h-3'} 
+      aria-hidden={'true'} 
+      xmlns={'http://www.w3.org/2000/svg'} 
+      fill={'none'} 
+      viewBox={'0 0 14 14'}>
+      <path 
+        stroke={'currentColor'} 
+        stroke-linecap={'round'} 
+        stroke-linejoin={'round'} 
+        stroke-width={'2'} 
+        d={'m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6'} />
+    </svg>
+  );
+}
+
+export function CloseSolidCircle() {
+  return (
+    <svg 
+      xmlns={'http://www.w3.org/2000/svg'}
+      viewBox={'0 0 24 24'}
+      fill={'currentColor'} 
+      className={'size-5'}>
+      <path
+        fillRule={'evenodd'}
+        d={'M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm-1.72 6.97a.75.75 0 1 0-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 13.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 12l1.72-1.72a.75.75 0 1 0-1.06-1.06L12 10.94l-1.72-1.72Z'}
+        clipRule={'evenodd'} />
+    </svg>
+  );
+}
